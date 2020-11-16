e100a96f7328037b588309b5d576c2916275ab85
John Heslop <johnheslop24@gmail.com>
2020-11-16 05:13:37 +0000

    first commit, fresh project with some ui deps

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import { preset } from 'vue-cli-plugin-vuetify-preset-rally/preset'
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  preset,
+  theme: {
+    themes: {
+      light: {
+        primary: '#ee44aa',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      },
+    },
+  },
+  icons: {
+    iconfont: 'md',
+  },
+});
